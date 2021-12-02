33fa317e78a4dc49c3d8b4326d07eb9d3ce39527
unknown <bhavyagoel1029@gmail.com>
2021-12-02 22:37:56 +0530

    adding tanishq

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
+    if request.method=='POST':
+        print(request.form['username'])
+        print(request.form['password'])
     return render_template('home.html')
 
 app.run(debug=True)
